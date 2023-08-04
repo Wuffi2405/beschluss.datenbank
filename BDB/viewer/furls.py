fbe7a84f05416cccfc5ce17eb7d4ea6a0c908750
Dennis Wuitz <dennish@wuitz.de>
2023-08-04 14:48:51 +0200

    created basic web endpoints

---
+++ BDB/viewer/furls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('documents', list_document, name='list_document'),
+    path('document/', view_document, name='cview_document'),
+    path('document/<str:document_id>', view_document, name='view_document'),
+]
--- BDB/viewer/views.py
+++ BDB/viewer/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+import json
+import os
+import glob
 
-# Create your views here.
+def view_document(request, document_id=None):
+    if document_id is None:
+        return redirect('list_document')
+    # check if file exists
+    if not os.path.exists(f'./data/{document_id}.json'):
+        return render(request, 'viewer/404.html', status=404)
+    # read json file from ./data/document_id.json
+    document = None
+    try:
+        document = json.load(open(f'./data/{document_id}.json'))
+    except:
+        return render(request, 'viewer/500.html', status=500)
+    return render(request, 'view_document.html', {'document': document})
+
+def list_document(request):
+    # get all files from ./data
+    documents = [ f.split('/')[-1].split('.')[0] for f in glob.glob('./data/*.json')]
+    return render(request, 'list_document.html', {'documents': documents})
