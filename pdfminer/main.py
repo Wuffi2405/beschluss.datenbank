3baeae43d92d2e4765d03c7b18e26a7a60ee9bf4
FraFrieFa <fabius2001@hotmail.de>
2023-08-05 18:42:33 +0200

    Full data

--- pdfminer/main.py
+++ pdfminer/main.py
@@ -13,74 +13,108 @@ from io import BytesIO
 
 import json
 
-data = []
-pages = extract_pages("sample.pdf")
+import os
 
-for page_layout in pages:
-    print(page_layout)
-    for layout_element in page_layout:
-        if isinstance(layout_element, LTTextBox):            
-            textboxFont = None
-            textboxSize = None
-            for line_element in layout_element._objs:                
-                if isinstance(line_element, LTTextLine):
-                    for character in line_element._objs:
-                        if isinstance(character, LTChar):
-                            if textboxFont == None:
-                                textboxFont = character.fontname
-                            if textboxSize == None:
-                                textboxSize = character.size
-                            if textboxFont != character.fontname:
-                                if not character.fontname.endswith("Italic"):
-                                    print("MISMATCH", textboxFont, character.fontname)
+from os import listdir
+from os.path import isdir, isfile
+from glob import glob
+
+
+
+def extract_file(indir, outdir):
+
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+
+    data = []
+    pages = extract_pages(indir)
+    for page_layout in pages:
+        print(page_layout)
+        for layout_element in page_layout:
+            if isinstance(layout_element, LTTextBox):            
+                textboxFont = None
+                textboxSize = None
+                for line_element in layout_element._objs:                
+                    if isinstance(line_element, LTTextLine):
+                        for character in line_element._objs:
+                            if isinstance(character, LTChar):
+                                if textboxFont == None:
+                                    textboxFont = character.fontname
+                                if textboxSize == None:
+                                    textboxSize = character.size
+                                if textboxFont != character.fontname:
+                                    if not character.fontname.endswith("Italic"):
+                                        print("MISMATCH", textboxFont, character.fontname)
+                                        #exit()
+                                if abs(textboxSize - character.size) > 0.01:
+                                    print("MISMATCH", textboxSize, character.size)
                                     #exit()
-                            if abs(textboxSize - character.size) > 0.01:
-                                print("MISMATCH", textboxSize, character.size)
-                                #exit()
-                else:
-                    print("PLEASE HANDLE UNKNOWN ELEMENT: ", line_element)
-                    exit()
-            data.append((layout_element.get_text(), textboxFont, round(textboxSize,2), layout_element.bbox))
+                    else:
+                        print("PLEASE HANDLE UNKNOWN ELEMENT: ", line_element)
+                        exit()
+                data.append((layout_element.get_text(), textboxFont, round(textboxSize,2), layout_element.bbox))
 
-font_map = {}
+    font_map = {}
 
-for x in data:
-    font_data = (x[1], x[2])
-    weight = len(x[0])
+    for x in data:
+        font_data = (x[1], x[2])
+        weight = len(x[0])
 
-    if font_data not in font_map:
-        font_map[font_data] = weight
-    else:
-        font_map[font_data] += weight
+        if font_data not in font_map:
+            font_map[font_data] = weight
+        else:
+            font_map[font_data] += weight
 
-content_data = max(font_map.items(), key = lambda x : x[1])[0]
+    content_data = max(font_map.items(), key = lambda x : x[1])[0]
 
-probably_content = [x for x in font_map if abs(x[1] - content_data[1]) < 0.1]
+    probably_content = [x for x in font_map if abs(x[1] - content_data[1]) < 0.1]
 
-beschlüsse = []
+    beschlüsse = []
 
-beschluss_element = ["", ""]
+    beschluss_element = ["", ""]
 
-for x in range(len(data)):
-    if (data[x][1], data[x][2]) not in probably_content:
-        if "Titel" in data[x][0]:
-            if (data[x+1][1], data[x+1][2]) not in probably_content:
-                if beschluss_element != ["",""]:
-                    beschlüsse.append(beschluss_element)
-                    beschluss_element = ["",""]
-                beschluss_element[0] = data[x+1][0].replace("\n"," ").strip()
-    if (data[x][1], data[x][2]) in probably_content:
-        if beschluss_element[0] != "":
-            if not data[x][0].strip().isdigit():
-                html_data = data[x][0].replace("-\n", "").replace("\n", " ").strip()
-                html_data.replace("\n", " ")
-                html_data.strip()
-                html_data = "<p>" + html_data + "</p>"
-                beschluss_element[1] += html_data
+    for x in range(len(data)):
+        if (data[x][1], data[x][2]) not in probably_content:
+            if "Titel" in data[x][0]:
+                if (data[x+1][1], data[x+1][2]) not in probably_content:
+                    if beschluss_element != ["",""]:
+                        beschlüsse.append(beschluss_element)
+                        beschluss_element = ["",""]
+                    beschluss_element[0] = data[x+1][0].replace("\n"," ").strip()
+        if (data[x][1], data[x][2]) in probably_content:
+            if beschluss_element[0] != "":
+                if not data[x][0].strip().isdigit():
+                    html_data = data[x][0].replace("-\n", "").replace("\n", " ").strip()
+                    html_data.replace("\n", " ")
+                    html_data.strip()
+                    html_data = "<p>" + html_data + "</p>"
+                    beschluss_element[1] += html_data
 
-if beschluss_element != ["",""]:
-    beschlüsse.append(beschluss_element)
+    if beschluss_element != ["",""]:
+        beschlüsse.append(beschluss_element)
 
-for id,e in enumerate(beschlüsse):
-    with open("../data/\""+ e[0] + ".json\"", "w") as file:
-        file.write(json.dumps({"title": e[0], "text_html": e[1]}, ensure_ascii=False))
+    for id,e in enumerate(beschlüsse):
+        with open(outdir + "/\""+ e[0] + ".json\"", "w") as file:
+            file.write(json.dumps({"title": e[0], "text_html": e[1]}, ensure_ascii=False))
+
+    # TODO Add Antragssteller, PDF_document, ID to json
+
+def extract_recursive(indir, outdir):
+    for x in listdir(indir):
+        if isdir(indir + "/" + x):
+            extract_recursive(indir + "/" + x, outdir + "/" + x)
+        if isfile(indir + "/" + x):
+            if x.endswith(".pdf"):
+                foldername = x[:-4]
+                print("Found pdf:" , indir + "/" + x)
+                print("Extracting to ", outdir + "/" + foldername)
+                extract_file(indir + "/" + x, outdir + "/" + foldername)
+
+#extract_recursive(raw_data_dir, data_dir)
+
+raw_data_dir = "../raw_data"
+data_dir = "../data"
+
+
+for x in glob("../raw_data/**/*.pdf"):
+    print(x)
