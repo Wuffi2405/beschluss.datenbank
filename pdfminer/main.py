380e8239eaa05130ca04eb92b3e940b51cfcdd34
FraFrieFa <fabius2001@hotmail.de>
2023-08-05 16:48:37 +0200

    Data

--- pdfminer/main.py
+++ pdfminer/main.py
@@ -3,15 +3,20 @@
 from pathlib import Path
 from typing import Iterable, Any
 
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+
+from pdfminer.converter import HTMLConverter
 from pdfminer.high_level import extract_pages
-from pdfminer.layout import LTTextBox, LTTextLine, LTChar, LTAnno
-
-
-pages = extract_pages("small.pdf")
+from pdfminer.layout import LTTextBox, LTTextLine, LTChar, LTAnno, LAParams
+from pdfminer.pdfpage import PDFPage
+from io import BytesIO
 
 data = []
 
+pages = extract_pages("sample.pdf")
+
 for page_layout in pages:
+    print(page_layout)
     for layout_element in page_layout:
         if isinstance(layout_element, LTTextBox):            
             textboxFont = None
@@ -34,41 +39,49 @@ for page_layout in pages:
                 else:
                     print("PLEASE HANDLE UNKNOWN ELEMENT: ", line_element)
                     exit()
-            data.append((layout_element.get_text().strip(), textboxFont, textboxSize))
+            data.append((layout_element.get_text(), textboxFont, round(textboxSize,2), layout_element.bbox))
 
-rml = []
-for i, d in enumerate(data):
-    if d[0].isdigit():
-        rml.append(i)
+font_map = {}
 
-for i in reversed(rml):
-    data.pop(i)
+for x in data:
+    font_data = (x[1], x[2])
+    weight = len(x[0])
 
-
-
-data.sort(key=lambda x: x[2])
-
-for d in reversed(data):
-    if len(d[0]) < 3:
-        print("ID: " + d[0])
-        break
-
-for d in reversed(data):
-    if len(d[0]) > 8:
-        print("Title: " + d[0])
-        break
-
-sorted_fontsize = {}
-for d in data:
-    if round(d[2], 1) not in sorted_fontsize:
-        sorted_fontsize[round(d[2], 1)] = d[0]
+    if font_data not in font_map:
+        font_map[font_data] = weight
     else:
-        sorted_fontsize[round(d[2], 1)] += d[0]
+        font_map[font_data] += weight
 
-most_common_fontsize = None
-most_common_fontsize_count = 0
-for k, v in sorted_fontsize.items():
-    if len(v) > most_common_fontsize_count:
-        most_common_fontsize_count = len(v)
-        most_common_fontsize = k
-print("Content: ", sorted_fontsize[most_common_fontsize])
+content_data = max(font_map.items(), key = lambda x : x[1])[0]
+
+probably_content = [x for x in font_map if abs(x[1] - content_data[1]) < 0.1]
+
+beschlüsse = []
+
+beschluss_element = ["", ""]
+
+for x in range(len(data)):
+    if (data[x][1], data[x][2]) not in probably_content:
+        if "Titel" in data[x][0]:
+            if (data[x+1][1], data[x+1][2]) not in probably_content:
+                if beschluss_element != ["",""]:
+                    beschlüsse.append(beschluss_element)
+                    beschluss_element = ["",""]
+                beschluss_element[0] = data[x+1][0].replace("\n"," ").strip()
+    if (data[x][1], data[x][2]) in probably_content:
+        if beschluss_element[0] != "":
+            if not data[x][0].strip().isdigit():
+                html_data = data[x][0].replace("-\n", "").replace("\n", " ").strip()
+                html_data.replace("\n", " ")
+                html_data.strip()
+                html_data = "<p>" + html_data + "</p>"
+                beschluss_element[1] += html_data
+
+if beschluss_element != ["",""]:
+    beschlüsse.append(beschluss_element)
+
+for id,e in enumerate(beschlüsse):
+    with open("../data/\""+ e[0] + ".txt\"", "w") as file:
+        file.write("{title:" + e[0]+",")
+        file.write("text: " + e[1] + "}")
+        
